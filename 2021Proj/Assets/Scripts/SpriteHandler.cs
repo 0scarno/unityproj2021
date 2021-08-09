c3c5b37a5262a3c82534f4ad2d19e4dda29fc8b9
oscar <oscarjemmott@gmail.com>
2021-08-09 17:29:03 +1000

    Test Cube, No More TAA, First Steps in Animation

--- 2021Proj/Assets/Scripts/SpriteHandler.cs
+++ 2021Proj/Assets/Scripts/SpriteHandler.cs
@@ -4,26 +4,53 @@ using UnityEngine;
 
 public class SpriteHandler : MonoBehaviour
 {
-    [SerializeField]
     private SpriteRenderer _spriteRenderer;
-
+    private Animator _animator;
+    
     // Start is called before the first frame update
     void Start()
     {
-
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _animator = GetComponent<Animator>();
     }
     // Update is called once per frame
     void Update()
     {
         float horiz = Input.GetAxisRaw("Horizontal");
-        if (horiz < 0)
+        float verti = Input.GetAxisRaw("Vertical");
+
+        if (_spriteRenderer!=true)
         {
-            _spriteRenderer.flipX = false;
+            Debug.LogError( "Sprite Renderer not found");
         }
-        else if (horiz > 0)
+        else
         {
-            _spriteRenderer.flipX = true;
+            
+            if (horiz < 0)
+            {
+                _spriteRenderer.flipX = false;
+            }
+            else if (horiz > 0)
+            {
+                _spriteRenderer.flipX = true;
+            }
         }
-        
+
+        if (_animator != true)
+        {
+            Debug.LogError("Animator not found");
+        }
+        else
+        {
+            if (horiz != 0 || verti != 0)
+            {
+                _animator.SetBool("isWalk", true);
+            }
+           else
+            {
+                _animator.SetBool("isWalk", false);
+            }
+        }
+
     }
 }
